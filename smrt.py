bbc9fe4d5e7fed5e788ea935435e04c2b3255079
Philippe Chataignon <philippe.chataignon@gmail.com>
2020-11-12 09:49:59 +0100

    Simplify smrt logic

--- smrt.py
+++ smrt.py
@@ -31,35 +31,30 @@ def main():
     args = parser.parse_args()
 
     logging.basicConfig(level=args.loglevel)
-
     net = Network(args.ip_address, args.host_mac, args.switch_mac)
     actions = Protocol.tp_ids
 
-    if args.vlan:
-        if args.vlan_member or args.vlan_tagged or args.delete:
-            if (args.delete):
-                v = Protocol.set_vlan(int(args.vlan), 0, 0, "")
-            else:
-                v = Protocol.set_vlan(int(args.vlan), ports2byte(args.vlan_member), ports2byte(args.vlan_tagged), args.vlan_name)
-            net.login(args.username, args.password)
-            l = [(actions["vlan"], v)]
-            header, payload = net.set(args.username, args.password, l)
-            print(*payload, sep="\n")
-
-        if args.vlan_pvid is not None:
-            net.login(args.username, args.password)
-            l = []
-            for port in ports2list(args.vlan_pvid):
-                if port != 0:
-                    l.append( (actions["pvid"], Protocol.set_pvid(args.vlan, port)) )
-            header, payload = net.set(args.username, args.password, l)
-            print(*payload, sep="\n")
-    else:
-        if args.action in actions:
-            header, payload = net.query(Protocol.GET, [(actions[args.action], b'')])
-            print(*payload, sep="\n")
-        else:
+    if args.action not in Protocol.tp_ids and not args.vlan:
             print("Actions:" , *actions.keys())
+    else:
+        net.login(args.username, args.password)
+        if args.vlan:
+            if args.vlan_member or args.vlan_tagged or args.delete:
+                if (args.delete):
+                    v = Protocol.set_vlan(int(args.vlan), 0, 0, "")
+                else:
+                    v = Protocol.set_vlan(int(args.vlan), ports2byte(args.vlan_member), ports2byte(args.vlan_tagged), args.vlan_name)
+                l = [(actions["vlan"], v)]
+
+            if args.vlan_pvid is not None:
+                l = []
+                for port in ports2list(args.vlan_pvid):
+                    if port != 0:
+                        l.append( (actions["pvid"], Protocol.set_pvid(args.vlan, port)) )
+            header, payload = net.set(args.username, args.password, l)
+        elif args.action in actions:
+            header, payload = net.query(Protocol.GET, [(actions[args.action], b'')])
+        print(*payload, sep="\n")
 
 if __name__ == "__main__":
     main()
